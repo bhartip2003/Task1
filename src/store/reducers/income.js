8c442ab270a0be16e01b23a7d4d61265524dfc04
Bharti Patel <bharti@volopay.co>
2024-11-20 15:53:31 +0530

    income reducer created

---
+++ src/store/reducers/income.js
@@ -0,0 +1,41 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import axios from "axios";
+
+
+const API_KEY = import.meta.env.VITE_API_STOCK;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+export const fetchIncomeStatement = createAsyncThunk('company/fetchIncomeStatement', async(ticker, {dispatch}) => {
+    const response = await axios.get(`${BASE_URL}/query?function=INCOME_STATEMENT&symbol=${ticker}&apikey=${API_KEY}`);
+   
+    dispatch(setIncomeStatement(response.data.annualReports));
+})
+
+
+const incomeSlice = createSlice({
+    name: 'income',
+    initialState: {
+        income: [],
+        loading: false,
+        error: null,
+    },
+    reducers: {
+        setIncomeStatement : (state, action) => {
+            state.income = action.payload
+        },
+        setLoading: (state, action) => {
+            state.loading= action.payload
+        },
+        setError: (state, action) => {
+            state.error= action.payload
+        }
+    },
+});
+
+export const {
+    setError,
+    setLoading,
+    setIncomeStatement
+} = incomeSlice.actions;
+
+export default incomeSlice.reducer;
