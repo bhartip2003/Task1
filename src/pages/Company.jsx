1b7db29881c833b3ac50120c946d859a79080708
Bharti Patel <bharti@volopay.co>
2024-11-20 15:23:48 +0530

    css fixes done in company overview section

--- src/pages/Company.jsx
+++ src/pages/Company.jsx
@@ -10,22 +10,22 @@ const Company = () => {
     <>
       <section className="container flex flex-col justify-center my-20 px-20 mx-28 gap-4">
         <h1 className="font-semibold text-3xl">{company.Name}</h1>
-        <p className="mt-10">{company.Description}</p>
-        <div className="flex flex-col ">
+        <p className="mt-10 text-gray-300">{company.Description}</p>
+        <div className="flex flex-col text-sm text-gray-300">
           <span className="font-light flex items-center gap-3">
-            <span>
+            <span className="text-gray-400">
               <TfiExchangeVertical />
             </span>
             {company.Exchange}
           </span>
           <span className="font-light flex items-center gap-3">
-            <span>
+            <span className="text-gray-400">
               <RiMapPin2Line />
             </span>
             {company.Country}
           </span>
           <span className="font-light flex items-center gap-3">
-            <span>
+            <span className="text-gray-400">
               <HiOutlineBuildingOffice2 />
             </span>
             {company.Industry}
