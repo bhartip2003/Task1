7919fffd9d6067d71a168bdc77a332459f2f648d
Bharti Patel <bharti@volopay.co>
2024-11-25 16:36:57 +0530

    product reducer created

---
+++ src/store/reducers/product.js
@@ -0,0 +1,41 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+
+const BASE_URL = import.meta.env.VITE_PRODUCT_URL;
+
+export const fetchProducts = createAsyncThunk(
+  "product/fetchProducts",
+  async (limit, skip, { dispatch }) => {
+      
+        const response = await axios.get(
+          `${BASE_URL}/products?limit=${limit}&skip=${skip}`
+        );
+      dispatch(setProducts(response.data.products));
+    }
+  
+);
+
+const productSlice = createSlice({
+  name: "product",
+  initialState: {
+    productData: null,
+    loading: false,
+    error: null,
+  },
+  reducers: {
+    setProducts: (state, action) => {
+      state.productData = action.payload;
+    },
+    setLoading: (state, action) => {
+      state.loading = action.payload;
+    },
+    setError: (state, action) => {
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { setError, setLoading, setProducts } = productSlice.actions;
+
+export default productSlice.reducer;
