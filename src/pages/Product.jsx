caf06dbc755f88ea7e00267c45aa253d87219c49
Bharti Patel <bharti@volopay.co>
2024-11-25 14:52:04 +0530

    product page structure created

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import {headers, headerTitle} from '../data/product.json'
+import axios from 'axios'
+
 
 const Product = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchProducts = async() => {
+            const response = await axios.get("https://dummyjson.com/products");
+            setData(response.data.products);
+        }
+
+        fetchProducts();
+    }, []);
+
   return (
     <div>
         <table>
             <thead>
-                
+                {headerTitle.map((header) => (
+                        <th className='font-semibold text-lg'>{header}</th>
+                    ))
+                }
             </thead>
             <tbody>
-
+                { data.map((item) => (
+                <tr key={item.id}>
+                    {headers.map((header) => (    
+                        <td>{item[header]}</td>
+                    ))
+                    }
+                </tr>
+                ))}
             </tbody>
         </table>
     </div>
