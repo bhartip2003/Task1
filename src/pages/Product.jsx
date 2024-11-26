7285e49c15c023ca3a6bfa87f46b0e84b5c21dfc
Bharti Patel <bharti@volopay.co>
2024-11-26 10:49:17 +0530

    products fetching by category implemented

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -5,22 +5,31 @@ import Table from "../components/Table";
 import { useDispatch, useSelector } from "react-redux";
 import { categorySelector } from "../store/selectors/categorySelector";
 import { fetchCategory } from "../store/reducers/category";
-import { fetchProducts } from "../store/reducers/product";
-import {productSelector} from "../store/selectors/productSelector";
+import { fetchProducts, fetchProductsByCategory, resetProducts } from "../store/reducers/product";
+import { productSelector } from "../store/selectors/productSelector";
 
 const Product = () => {
-  const [data, setData] = useState([]);
   const categoryData = useSelector(categorySelector);
   const productData = useSelector(productSelector);
   const dispatch = useDispatch();
-
+  const [category, setCategory] = useState(null);
   const limit = 15;
 
-
   useEffect(() => {
-    dispatch(fetchProducts({limit:limit, skip:10}));
+    dispatch(resetProducts());
+    
+    if(category == null)
+      dispatch(fetchProducts({ limit: limit, skip: 0 }));
+    else {
+      dispatch(fetchProductsByCategory({category: category, limit: limit, skip: 0}));
+    }
+
     dispatch(fetchCategory());
   }, []);
+
+  const handleButtonClick = (selectedCategory) => {
+    setCategory(selectedCategory);
+  }
 
   return (
     <div className="container flex flex-col justify-center items-center my-32 gap-y-10">
@@ -29,22 +38,27 @@ const Product = () => {
       {/* product category */}
       <div className="flex gap-x-5">
         {categoryData.length > 0 ? (
-          categoryData
-            .slice(0, 5)
-            .map((category) => (
+          categoryData.slice(0, 5).map((category) => (
+            <div onClick={() => handleButtonClick(category.slug)}>
               <Button
                 key={category.slug}
                 type={category.slug}
                 name={category.name}
               />
-            ))
+            </div>
+          ))
         ) : (
           <p>Loading Categories...</p>
         )}
       </div>
-        
-        {/* product table */}
-      <Table data={productData} headers={headers} headerTitle={headerTitle} toggle={false} />
+
+      {/* product table */}
+      <Table
+        data={productData}
+        headers={headers}
+        headerTitle={headerTitle}
+        toggle={false}
+      />
     </div>
   );
 };
--- src/store/reducers/product.js
+++ src/store/reducers/product.js
@@ -1,32 +1,42 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-
 const BASE_URL = import.meta.env.VITE_PRODUCT_URL;
 
 export const fetchProducts = createAsyncThunk(
   "product/fetchProducts",
-  async ({limit, skip}, { dispatch }) => {
-        
-        const response = await axios.get(
-          `${BASE_URL}/products?limit=${limit}&skip=${skip}`
-        );
-      dispatch(setProducts(response.data.products));
-    }  
+  async ({ limit, skip }, { dispatch }) => {
+    const response = await axios.get(
+      `${BASE_URL}/products?limit=${limit}&skip=${skip}`
+    );
+    dispatch(setProducts(response.data.products));
+  }
 );
 
+export const fetchProductsByCategory = createAsyncThunk(
+  "product/fetchProductsByCategory",
+  async ({ category, limit, skip }, { dispatch }) => {
+    const response = await axios.get(
+      `${BASE_URL}/products/category/${category}?limit=${limit}&skip=${skip}`
+    );
 
+    dispatch(setProducts(response.data.products));
+  }
+);
 
 const productSlice = createSlice({
   name: "product",
   initialState: {
-    productData: null,
+    productData: [],
     loading: false,
     error: null,
   },
   reducers: {
+    resetProducts: (state, action) => {
+      state.productData = [];
+    },
     setProducts: (state, action) => {
-      state.productData = action.payload;
+      state.productData = [...productData, ...action.payload];
     },
     setLoading: (state, action) => {
       state.loading = action.payload;
@@ -37,6 +47,6 @@ const productSlice = createSlice({
   },
 });
 
-export const { setError, setLoading, setProducts } = productSlice.actions;
+export const { setError, setLoading, setProducts, resetProducts } = productSlice.actions;
 
 export default productSlice.reducer;
