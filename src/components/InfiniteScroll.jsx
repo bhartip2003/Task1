dd4bfc1d8c96e2bc7f744227cee2cf6f2e41a718
Bharti Patel <bharti@volopay.co>
2024-11-26 12:57:10 +0530

    infinite scroll component created

---
+++ src/components/InfiniteScroll.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react'
+import { useInView } from 'react-intersection-observer'
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchProducts } from '../store/reducers/product';
+import { loadingSelector, productSelector } from '../store/selectors/productSelector';
+import Loading from "./Loading";
+
+const InfiniteScroll = () => {
+    const {ref, inView} = useInView();
+    const dispatch = useDispatch();
+    const productData = useSelector(productSelector);
+    const loading = useSelector(loadingSelector);
+
+    const pageNumber = productData.length / 15;
+    useEffect(() => {
+        if(inView && !loading){
+            dispatch(fetchProducts({category, limit: 15, skip: pageNumber * 15}));
+        }
+    }, [inView, loading]);
+
+  return (
+    <div ref={ref}>{loading && <Loading />}</div>
+  )
+}
+
+export default InfiniteScroll
