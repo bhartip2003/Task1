8b15e85ad18a71df6b8036fea631dffe28762036
Bharti Patel <bharti@volopay.co>
2024-12-01 15:25:34 +0530

    api call done to fetch single product item

--- src/store/reducers/product.js
+++ src/store/reducers/product.js
@@ -3,6 +3,32 @@ import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_PRODUCT_URL;
 
+const productSlice = createSlice({
+  name: "product",
+  initialState: {
+    productData: [],
+    loading: false,
+    error: null,
+  },
+  reducers: {
+    resetProducts: (state, action) => {
+      state.productData = [];
+    },
+    setProducts: (state, action) => {
+      state.productData = [...state.productData, ...action.payload];
+    },
+    setProductItem: (state, action) => {
+      state.productData = [...state.productData, ...action.payload];
+    },
+    setLoading: (state, action) => {
+      state.loading = action.payload;
+    },
+    setError: (state, action) => {
+      state.error = action.payload;
+    },
+  },
+});
+
 export const fetchProducts = createAsyncThunk(
   "product/fetchProducts",
   async ({ category=null, limit, skip }, { dispatch }) => {
@@ -19,31 +45,12 @@ export const fetchProducts = createAsyncThunk(
   }
 );
 
+export const fetchOneProduct = createAsyncThunk("product/fetchOneProduct", async(id, {dispatch}) => {
+  const response = await axios.get(`${BASE_URL}/products/${id}`);
+  dispatch(setProductItem(response.data));
+})
 
-const productSlice = createSlice({
-  name: "product",
-  initialState: {
-    productData: [],
-    loading: false,
-    error: null,
-  },
-  reducers: {
-    resetProducts: (state, action) => {
-      state.productData = [];
-    },
-    setProducts: (state, action) => {
-      state.productData = [...state.productData, ...action.payload];
-    },
-    setLoading: (state, action) => {
-      state.loading = action.payload;
-    },
-    setError: (state, action) => {
-      state.error = action.payload;
-    },
-  },
-});
-
-export const { setError, setLoading, setProducts, resetProducts } =
+export const { setError, setLoading, setProducts, setProductItem, resetProducts } =
   productSlice.actions;
 
 export default productSlice.reducer;
